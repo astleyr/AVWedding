51d8a4300f26d6307dc9bc9b59e8a7f6b57f4a55
Astley Rampersad <astleyrampersad@gmail.com>
2015-10-20 15:24:35 +0200

    First Commit

---
+++ wedding.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'pony'
+
+get '/' do
+    erb :index
+end
+
+post '/' do
+    name = params[:name]
+    mail = params[:email]
+    body = params[:body]
+    Pony.mail(:to => 'astleyrampersad@gmail.com', :from => mail, :subject => 'Wedding' + name, :body => body)
+    redirect to '/'
+end
+
+
+error do
+  @error = request.env['sinatra_error']
+  erb :error
+end
+
+not_found do
+  erb :page_missing
+end
+
